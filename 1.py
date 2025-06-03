133cbd1087e71b5d5aa5ba8f86ab94ec5c3c2182
Админ <admin@MacBook-Air-Abstract.local>
2025-06-03 12:31:14 +0500

    first commit

---
+++ 1.py
@@ -0,0 +1,77 @@
+#print("Hello, World!")
+
+#if 5 > 2:
+    #print("Five is greater than two!")
+
+#x = 5
+#y = "Hello, World!"
+#print (x, y)
+
+#x = 5  #int
+#y = "Di"  #str
+#print(x)
+#print(y)
+
+#x = str("Hello") 
+#y = int(3)    
+#z = float(3)
+#print(x,y,z)
+
+#x, y, z = "Orange", "Banana", "Cherry"
+#print(x)
+#print(y)
+#print(z)
+
+#x = "awesome"  -?
+#def myfunc():
+  #global x
+  #x = "fantastic"
+
+#myfunc()
+
+#print("Python is " + x)
+
+#x = 1  
+#y = 2.8  
+#z = 1j
+#print(type(x))
+#print(type(y))
+#print(type(z))
+
+#import random
+#print(random.randrange(1, 10))
+
+#a = "Hello, World!"
+#print(a[6])
+
+
+#a = "Hello, World!"
+#print(len(a)) #количество
+
+#txt = "The best things in life are free!"
+#if "free" in txt:
+ # print("Yes, 'free' is present.")
+
+#b = "Hello, World!"
+#print(b[-5:-2])
+
+#a = "Hello, World!"
+#print(a.upper())
+#print(a.lower())
+#print(a.strip())
+#print(a.replace("H", "J"))
+#print(a.split(","))
+
+#a = "Hello"
+#b = "World"
+#c = a + b
+#print(c)
+
+#price = 59
+#txt = f"The price is {price:.3f} dollars"
+#txt = f"The price is {20 * 59} dollars"
+#print(txt)
+
+## Python - Escape Characters...?
+#txt = "We are the so-called \"Vikings\" from the north."
+
